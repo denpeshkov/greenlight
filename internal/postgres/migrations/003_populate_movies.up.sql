f89f96acf9a0f9036188d83a14dca7a9e1b6c984
Denis Peshkov <denpeshkov98@gmail.com>
2024-01-18 20:06:28 +0400

    Add populate movies migrations

---
+++ internal/postgres/migrations/003_populate_movies.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO movies (title, release_date, runtime, genres) VALUES ('Moana', '2016-10-23', 105, '{"animation","adventure","comedy"}');
+INSERT INTO movies (title, release_date, runtime, genres) VALUES ('Shattered Island', '2010-02-18', 138, '{"thriller","mystery"}');
+INSERT INTO movies (title, release_date, runtime, genres) VALUES ('Arcane', '2021-10-06', 41, '{"action","animation"}');
