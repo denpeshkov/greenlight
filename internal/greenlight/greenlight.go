54d4c18e676d833db3122c59f91a0957f07e1f34
Denis Peshkov <denpeshkov98@gmail.com>
2024-02-18 22:13:58 +0400

    app version from git commit

--- internal/greenlight/greenlight.go
+++ internal/greenlight/greenlight.go
@@ -1,3 +1,30 @@
 package greenlight
 
-const AppVersion = "1.0.0"
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+var AppVersion = Version()
+
+func Version() string {
+	var revision string
+	var modified bool
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, s := range bi.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				revision = s.Value
+			case "vcs.modified":
+				if s.Value == "true" {
+					modified = true
+				}
+			}
+		}
+	}
+	if modified {
+		return fmt.Sprintf("%s-dirty", revision)
+	}
+	return revision
+}
